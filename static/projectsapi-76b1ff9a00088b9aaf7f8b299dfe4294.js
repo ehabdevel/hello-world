1e7cab867bd3e2190c4d691a49d7ae5e110dfbec
Ehab Ashour <ehabdevel@gmail.com>
2018-07-14 18:54:18 +0200

    Updates

---
+++ static/projectsapi-76b1ff9a00088b9aaf7f8b299dfe4294.js
@@ -0,0 +1,313 @@
+const projects = [
+
+	{
+		'id': '001',
+		'name': 'Rohanou Hotel',
+		'link': 'rohanou.com',
+		'image': 'rohanou.webp',
+		'status': 'active',
+		'services': 'Host - Develope - Design',
+		'description': 'Rohanou Resort hotel located on the south of Quseir - Marsa Alam High Way, Red Sea',
+		'startyear': '2014',
+		'endyear': 'present'
+	},
+	{
+		'id': '002',
+		'name': 'Chamber of Commerce',
+		'link': 'coc-redsea.org',
+		'image': 'cocredc.webp',
+		'status': 'closed',
+		'services': 'Host',
+		'description': 'The Chamber of Commerce of Red Sea',
+		'startyear': '2011',
+		'endyear': '2018'
+	},
+	{
+		'id': '003',
+		'name': 'Twins Diving',
+		'link': 'twins-diving.com',
+		'image': 'twinsdiving.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Twins Diving Center at Soam bay.',
+		'startyear': '2010',
+		'endyear': '2013'
+	},
+	{
+		'id': '004',
+		'name': 'Yassmina Tours',
+		'link': 'yassminatours.com',
+		'image': 'yassmina.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Yassmina Tours Company - Tourism Company in Hurghada',
+		'startyear': '2012',
+		'endyear': '2015'
+	},
+	{
+		'id': '005',
+		'name': 'Makka Shipping',
+		'link': 'makkashipping.com',
+		'image': 'makkashipping.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Makka Shipping Company - Shipping Company in Hurghada',
+		'startyear': '2012',
+		'endyear': '2017'
+	},
+	{
+		'id': '006',
+		'name': 'Ice Princess',
+		'link': 'iceprincess-redsea.com',
+		'image': 'iceprincess.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Ice Princess Diving Boat Hurghada',
+		'startyear': '2013',
+		'endyear': '2016'
+	},
+	{
+		'id': '007',
+		'name': 'E-Reading Skills',
+		'link': 'e-readingskills.com',
+		'image': 'ereading.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'E-Reading Skills was for a student preparing her MBA, South Valley University, Hurghada Branch (Education)',
+		'startyear': '2013',
+		'endyear': '2016'
+	},
+	{
+		'id': '0010',
+		'name': 'Hurghada Journal',
+		'link': 'hurghadajournal.com',
+		'image': 'hurghadajournal.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Hurghada Journal and city community',
+		'startyear': '2010',
+		'endyear': '2017'
+	},
+	{
+		'id': '0011',
+		'name': 'Al-Madyh',
+		'link': 'almadyh.com',
+		'image': 'almadyh.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'A religion website for a famous Imam',
+		'startyear': '2010',
+		'endyear': '2012'
+	},
+	{
+		'id': '0012',
+		'name': 'Smart Stationry',
+		'link': 'smartstationry.com',
+		'image': 'smart.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Smart Stationry book shop',
+		'startyear': '2010',
+		'endyear': '2011'
+	},
+	{
+		'id': '0013',
+		'name': 'Smart Shop-eg',
+		'link': 'smartshop-eg.com',
+		'image': 'smart.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Smart Shop-eg book shop',
+		'startyear': '2010',
+		'endyear': '2013'
+	},
+	{
+		'id': '0014',
+		'name': 'Eshtta Dancing',
+		'link': 'eshttadancing.com',
+		'image': 'eshtta.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Eshtta Dancing is a tourism bazaar shop',
+		'startyear': '2012',
+		'endyear': '2013'
+	},
+	{
+		'id': '0015',
+		'name': 'Wakawaka Safari',
+		'link': 'wakawaka-safari.com',
+		'image': 'wakawaka.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Wakawaka Safari and tourism Company',
+		'startyear': '2012',
+		'endyear': '2013'
+	},
+	{
+		'id': '0016',
+		'name': 'Hurghada News',
+		'link': 'hurghadanews.com',
+		'image': 'hurghadanews.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Hurghada News',
+		'startyear': '2013',
+		'endyear': 'present'
+	},
+	{
+		'id': '0017',
+		'name': 'At-tayyib',
+		'link': 'at-tayyib.com',
+		'image': 'nolonger.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'At-tayyib website for books and translations (Education)',
+		'startyear': '2013',
+		'endyear': '2015'
+	},
+	{
+		'id': '0018',
+		'name': 'School Radio',
+		'link': 'sis-radio.com',
+		'image': 'sunriseradio.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'A School Radio',
+		'startyear': '2013',
+		'endyear': '2014'
+	},
+	{
+		'id': '0021',
+		'name': 'E-Mind Mapping Reading Comprehension',
+		'link': 'emmrc.org',
+		'image': 'emmrc.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'A university doctor preparing for a doctorate, South Valley University, Hurghada Branch (Education)',
+		'startyear': '2013',
+		'endyear': '2016'
+	},
+	{
+		'id': '0022',
+		'name': 'Elmasrya Track',
+		'link': 'elmasrya-track.com',
+		'image': 'elmasrya-track.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Elmasrya Track Marine Company',
+		'startyear': '2014',
+		'endyear': '2016'
+	},
+	{
+		'id': '0023',
+		'name': 'Geant Training',
+		'link': 'geant-training.com',
+		'image': 'geant.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Geant Training Center (Education)',
+		'startyear': '2014',
+		'endyear': '2017'
+	},
+	{
+		'id': '0024',
+		'name': 'Porto Travel Service',
+		'link': 'portotravel-service.com',
+		'image': 'portotravel-service.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Porto Travel Service, tourism and transportation Company',
+		'startyear': '2015',
+		'endyear': '2017'
+	},
+	{
+		'id': '0025',
+		'name': 'A2Z Marine',
+		'link': 'a2z-marine.com',
+		'image': 'a2zmarine.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'A2Z Marine Marine Company',
+		'startyear': '2015',
+		'endyear': '2016'
+	},
+	{
+		'id': '0019',
+		'name': 'Truth As Seen',
+		'link': 'truthasseen.com',
+		'image': 'nolonger.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Truth as seen, association.',
+		'startyear': '2013',
+		'endyear': '2014'
+	},
+	{
+		'id': '0020',
+		'name': 'Expats In Egypt Movement',
+		'link': 'expatsinegyptmovement.org',
+		'image': 'nolonger.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Expats In Egypt Movement, association.',
+		'startyear': '2013',
+		'endyear': '2014'
+	},
+	{
+		'id': '008',
+		'name': 'MAS Globe',
+		'link': 'masglobe.com',
+		'image': 'nolonger.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'MAS Globe trading Company Hurghada',
+		'startyear': '2010',
+		'endyear': '2012'
+	},
+	{
+		'id': '009',
+		'name': 'IT Red Sea',
+		'link': 'itredsea.net',
+		'image': 'nolonger.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'itredsea.net for computer parts sales',
+		'startyear': '2010',
+		'endyear': '2013'
+	},
+	{
+		'id': '0026',
+		'name': 'Sea Holidays',
+		'link': 'seaholidays.de',
+		'image': 'nolonger.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Sea Holidays tourism Company',
+		'startyear': '2015',
+		'endyear': '2016'
+	},
+	{
+		'id': '0027',
+		'name': 'Hurghada Bulletin',
+		'link': 'hurghadabulletin.com',
+		'image': 'nolonger.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Hurghada Bulletin and city community',
+		'startyear': '2016',
+		'endyear': '2017'
+	},
+	{
+		'id': '0028',
+		'name': 'Akhty',
+		'link': 'akhty.com',
+		'image': 'nolonger.webp',
+		'status': 'closed',
+		'services': 'Host - Develope - Design',
+		'description': 'Akhty for web hosting',
+		'startyear': '2016',
+		'endyear': '2017'
+	}
+];
+
+export default projects;
